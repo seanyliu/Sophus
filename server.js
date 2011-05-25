85b0b7bc627f964392e50d32ce5d310b9fa2115f
Sean Liu <sean.y.liu@gmail.com>
2011-05-25 01:20:20 -0700

    clean up server.js

--- server.js
+++ server.js
@@ -5,44 +5,46 @@ var sys = require("sys"),
     fs = require("fs"),
     events = require("events");
 
+/**
+ * Load any static files based on the uri.
+ * Source:
+ * http://net.tutsplus.com/tutorials/javascript-ajax/..
+ * learning-serverside-javascript-with-node-js/
+ * With bug corrections in response methods
+ */
 function load_static_file(uri, response) {
-	var filename = path.join(process.cwd(), "client", uri);
-	path.exists(filename, function(exists) {
-		if(!exists) {
-			response.writeHead(404, {"Content-Type": "text/plain"});
-			response.write("404 Not Found\n");
-			response.end();
-			return;
-		}
+  var filename = path.join(process.cwd(), "client", uri); // added "client"
+  path.exists(filename, function(exists) {
 
-                if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-		fs.readFile(filename, "binary", function(err, file) {
-			if(err) {
-				response.writeHead(500, {"Content-Type": "text/plain"});
-				response.write(err + "\n");
-				response.end();
-				return;
-			}
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
 
-			response.writeHead(200);
-			response.write(file, "binary");
-			response.end();
-		});
-	});
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+
+  });
 }
 
 var server = http.createServer(function(request, response) {
-
-    var uri = url.parse(request.url).pathname;
-    if(uri === "/stream") {
-
-      // http://net.tutsplus.com/tutorials/javascript-ajax/learning-serverside-javascript-with-node-js/	
-
-    }
-    else {
-    	load_static_file(uri, response);
-    }
+  var uri = url.parse(request.url).pathname;
+  // parse uri for interesting things.
+  // e.g. if (uri === "/stream") ...
+  load_static_file(uri, response);
 });
 
 server.listen(process.env.PORT || 8001);
