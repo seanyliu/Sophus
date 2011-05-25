ae89a3958afbdddf3eba94d07e3f399dcae6c664
Sean Liu <sean.y.liu@gmail.com>
2011-05-25 00:52:46 -0700

    actually committing server.js file with client loader

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
-var http = require('http');
- 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n");
-});
- 
-server.listen(process.env.PORT || 8001);
+var sys = require("sys"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    events = require("events");
+
+function load_static_file(uri, response) {
+	var filename = path.join(process.cwd(), 'client', uri);
+	path.exists(filename, function(exists) {
+		if(!exists) {
+			response.sendHeader(404, {"Content-Type": "text/plain"});
+			response.write("404 Not Found\n");
+			response.close();
+			return;
+		}
+
+		fs.readFile(filename, "binary", function(err, file) {
+			if(err) {
+				response.sendHeader(500, {"Content-Type": "text/plain"});
+				response.write(err + "\n");
+				response.close();
+				return;
+			}
+
+			response.sendHeader(200);
+			response.write(file, "binary");
+			response.close();
+		});
+	});
+}
+
+http.createServer(function(request, response) {
+    var uri = url.parse(request.url).pathname;
+    if(uri === "/stream") {
+      // see: http://net.tutsplus.com/tutorials/javascript-ajax/learning-serverside-javascript-with-node-js/
+    }
+    else {
+    	load_static_file(uri, response);
+    }
+}).listen(8080);
+
+sys.puts("Server running at http://localhost:8080/");
+
