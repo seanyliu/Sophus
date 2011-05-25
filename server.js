7374e68e6bddc2984675146727db5d86960f7688
Sean Liu <sean.y.liu@gmail.com>
2011-05-25 01:00:03 -0700

    updated the port on server.js

--- server.js
+++ server.js
@@ -2,33 +2,43 @@ var sys = require("sys"),
     http = require("http"),
     url = require("url"),
     path = require("path"),
-    fs = require("fs");
+    fs = require("fs"),
+    events = require("events");
+
+function load_static_file(uri, response) {
+	var filename = path.join(process.cwd(), uri);
+	path.exists(filename, function(exists) {
+		if(!exists) {
+			response.sendHeader(404, {"Content-Type": "text/plain"});
+			response.write("404 Not Found\n");
+			response.close();
+			return;
+		}
+
+		fs.readFile(filename, "binary", function(err, file) {
+			if(err) {
+				response.sendHeader(500, {"Content-Type": "text/plain"});
+				response.write(err + "\n");
+				response.close();
+				return;
+			}
+
+			response.sendHeader(200);
+			response.write(file, "binary");
+			response.close();
+		});
+	});
+}
 
 http.createServer(function(request, response) {
     var uri = url.parse(request.url).pathname;
-    var filename = path.join(process.cwd(), uri);
-    path.exists(filename, function(exists) {
-    	if(!exists) {
-    		response.sendHeader(404, {"Content-Type": "text/plain"});
-    		response.write("404 Not Found\n");
-    		response.close();
-    		return;
-    	}
+    if(uri === "/stream") {
 
-    	fs.readFile(filename, "binary", function(err, file) {
-    		if(err) {
-    			response.sendHeader(500, {"Content-Type": "text/plain"});
-    			response.write(err + "\n");
-    			response.close();
-    			return;
-    		}
+      // http://net.tutsplus.com/tutorials/javascript-ajax/learning-serverside-javascript-with-node-js/	
 
-    		response.sendHeader(200);
-    		response.write(file, "binary");
-    		response.close();
-    	});
-    });
-}).listen(8080);
-
-sys.puts("Server running at http://localhost:8080/");
+    }
+    else {
+    	load_static_file(uri, response);
+    }
+}).listen(process.env.PORT || 8001);
 
